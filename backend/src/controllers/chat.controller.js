c6659bca1b6d621374a577407eb5742dde6f448a
Haroon Amin <aminharoon699@gmail.com>
2026-04-05 11:08:47 +0530

    update sendMessage function to create chat and message records with AI response

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,13 +1,26 @@
 
 import { generateResponse, generateChatTittle } from "../services/ai.service.js"
 import ApiResponse from "../utils/ApiResponse.js"
+import chatModel from "../models/chat.model.js"
+import messageModel from '../models/message.model.js'
+
 const sendMessage = async (req, res) => {
     const { message } = req.body
 
     const title = await generateChatTittle(message)
     const result = await generateResponse(message)
 
-    res.status(200).json(new ApiResponse(200, { title, result: result }))
+    const chat = await chatModel.create({
+        user: req.user._id,
+        tittle: title
+    })
+    const messages = await messageModel.create({
+        chat: chat._id,
+        content: result,
+        role: "ai"
+    })
+
+    res.status(201).json(new ApiResponse(201, { chat, response: messages }))
 
 }
 
